4f2026efa15a5efb675c1e114fb5153243790dd9
Guillermo Calvo <guillermocalvo@yahoo.com>
2018-04-25 12:04:00 +0900

    Install pre-compiled kcov binary instead of compiling from source

--- INSTALL-KCOV.sh
+++ INSTALL-KCOV.sh
@@ -5,15 +5,18 @@ echo Installing Kcov...
 set -x
 
 # Install from source code
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
-tar xzf master.tar.gz
-cd kcov-master
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ../..
-rm -rf kcov-master
+# wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+# tar xzf master.tar.gz
+# cd kcov-master
+# mkdir build
+# cd build
+# cmake ..
+# make
+# sudo make install
+# cd ../..
+# rm -rf kcov-master
+
+# Install from pre-compiled binary (kcov version 36)
+sudo cp test/kcov /usr/local/bin
 
 echo Installed `kcov --version`
